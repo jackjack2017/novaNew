1ba8b1b376d8ba42a6baf60de115b33d926cad4f
jackjack2017 <eugenkanev@gmail.com>
2018-06-18 17:37:37 +0300

    start commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://local.nowanew/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://local.nowanew/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/novaNew/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/novaNew/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/novaNew/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/novaNew/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/novaNew/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/novaNew/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/novaNew/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/novaNew/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/novaNew/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/novaNew/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/novaNew/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'novaNew');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
